f19eede1cdc2202184bf868b2748ac5e24d1a5f1
Nick Field <heyimnick@live.com>
2016-10-19 15:01:38 +0100

    Changed classnames to conform to BEM form

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
+    if (x.className === "nav") {
         x.className += " responsive";
     } else {
-        x.className = "topnav";
+        x.className = "nav";
     }
 }
