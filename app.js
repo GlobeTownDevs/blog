c0290f152cc5397ec11dc5d07e89e6e95dc561d9
lucymonie <lucy.monie.hall@gmail.com>
2016-10-19 16:32:15 +0100

    adds scrolling function to js file

---
+++ app.js
@@ -0,0 +1,7 @@
+var toFac = document.getElementById("to-fac");
+var fac = document.getElementById("fac");
+
+toFac.addEventListener("click", function(e) {
+  e.preventDefault();
+  fac.scrollIntoView();
+});
